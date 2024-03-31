0f55fc985fbff1df22bc1e47ca9f5f3e1a02c6d9
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-03-31 20:52:42 +0530

    Values

---
+++ Values/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// String
+	fmt.Println("go" + "lang")
+
+	// Integer and float
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	// Booleans
+	// '&&' AND opt.
+	fmt.Println(true && false)
+	fmt.Println(false && false)
+	fmt.Println(true && true)
+
+	// '||' OR opt.
+	fmt.Println(true || false)
+	fmt.Println(false || false)
+	fmt.Println(true || true)
+
+	// '!'
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
