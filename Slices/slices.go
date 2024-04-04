781b36f551b4bd05dc723d6def4bd264f65a2ebc
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-04-04 22:53:03 +0530

    Slices

---
+++ Slices/slices.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	//fmt.Println("Slices")
+
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	//fmt.Printf("Type of fruitlist is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+	//fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:])
+	//fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3])
+	//fmt.Println(fruitList)
+
+	fruitList = append(fruitList[:3])
+	//fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 445
+	highScores[3] = 555
+
+	highScores = append(highScores, 555,666,777)
+
+	//fmt.Println(highScores)
+
+	sort.Ints(highScores)
+	//fmt.Println(highScores)
+	//fmt.Println(sort.IntsAreSorted(highScores))
+
+	// how to remove a value from slices based on index
+
+	var courses = []string{"reactjs", "javascript", "swift", "python", "ruby"}
+	fmt.Println(courses)
+	var index int = 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
+}
