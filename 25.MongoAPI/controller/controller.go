c896cefe6fe9b75d1943d0acb8c503421670fa82
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-06-07 23:17:06 +0530

    .

---
+++ 25.MongoAPI/controller/controller.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://learning:piyush@cluster0.dasyz3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// MOST IMPORTANT
+var collection *mongo.Collection
+
+// connect with mongoDB
+
+func init() {
+	//client option
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	//connect to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	//collection instance
+	fmt.Println("Collection instance is ready")
+}
