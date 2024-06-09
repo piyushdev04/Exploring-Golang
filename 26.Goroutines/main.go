fbe32f09d3ae18d7de338ada730fc23c95fc214d
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-06-09 21:01:10 +0530

    .

--- 26.Goroutines/main.go
+++ 26.Goroutines/main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var signals = []string{"test"}
+
+var wg sync.WaitGroup //pointer
+
+var mut sync.Mutex //pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	websitelist := []string{
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+	fmt.Println(signals)
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+		
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 	}
 }
